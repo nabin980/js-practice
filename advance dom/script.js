0a16d7cb1cc2c0cb5f315f640b5a6a62f1a7a4af
Nabin Bohara <olinaveen98@gmail.com>
2024-11-13 22:04:00 +0545

    lazy loading an revel sections

--- advance dom/script.js
+++ advance dom/script.js
@@ -238,3 +238,52 @@ const headerObserver = new IntersectionObserver(stickyNav,{
 })
 
 headerObserver.observe(header);
+
+// Revele sections
+const allSection = document.querySelectorAll('.section');
+
+
+const revelSection = function(entries,observer){
+const [entry] = entries;
+
+if(!entry.isIntersecting) return;
+entry.target.classList.remove('section--hidden');
+observer.unobserve(entry.target);
+}
+
+
+const sectionObserver = new IntersectionObserver(revelSection,{
+root:null,
+threshold:0.15,
+})
+
+allSection.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+})
+
+// Lazy loading
+
+const imageTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries,observer){
+  const [entry] = entries;
+  console.log(entry);
+  if(!entry.isIntersecting) return;
+  
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  
+  entry.target.addEventListener('load',function(){
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target);
+}
+
+const imgObserver = new IntersectionObserver(loadImg,{
+root:null,
+threshold:0,
+rootMargin:"200px",
+})
+
+imageTargets.forEach(img => imgObserver.observe(img));
